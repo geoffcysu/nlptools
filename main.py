8fc5f54e8bd8252e73b27648b968bb7b1ab3c745
Geoffr Su <geoffcysu@gmail.com>
2023-08-27 21:20:04 +0800

    first successful implimentation of printing SyntaxTree

--- main.py
+++ main.py
@@ -94,6 +94,53 @@ class SyntaxTree(RoseTree):
             self.addChild(child)
     def __repr__(self):
         return f"Tree({self.content},{self._children})"
+    
+    def ppStr(self, layerLength=6)->str:
+        def render(tree:RoseTree)->list[str]:
+            if len(tree.children)==0:
+                return [str(tree.content)]
+            else:
+                contLen = len(tree.content)
+                padding = contLen-layerLength+1 if contLen>=layerLength else 0
+                        # +1 is an additional "_" character to separate parent and child nodes
+                contentStr = str(tree.content).ljust(layerLength+padding, '_')
+                newLayerLength = layerLength+padding
+
+                def prependingFirstLeaf(leaf:str)->str:
+                    return contentStr + leaf
+                def prependingHead(leaf:str)->str:
+                    return '|'.ljust(newLayerLength, '_') + leaf
+                def prependingNonchildLeaf(leaf:str)->str:
+                    return '|'.ljust(newLayerLength, ' ') + leaf
+                def prependingEnd(leaf:str)->str:
+                    return ' '.ljust(newLayerLength, ' ') + leaf
+                
+                def renderFirstChild(t:RoseTree)->list[str]:
+                    allLeaves = render(t)
+                    return [ prependingFirstLeaf(allLeaves[0])
+                        , *map(prependingNonchildLeaf,allLeaves[1:])]
+                def renderMiddleChildren(t:RoseTree)->list[str]:
+                    allLeaves = render(t)
+                    return [ prependingHead(allLeaves[0])
+                        , *map(prependingNonchildLeaf,allLeaves[1:])]
+                def renderLastChild(t:RoseTree)->list[str]:
+                    allLeaves = render(t)
+                    return [ prependingHead(allLeaves[0])
+                        , *map(prependingEnd,allLeaves[1:])]
+                def renderSoleChild(t:RoseTree)->list[str]:
+                    allLeaves = render(t)
+                    return [ prependingFirstLeaf(allLeaves[0])
+                        , *map(prependingEnd,allLeaves[1:])]
+                
+                if len(tree.children) > 1:
+                    return [ *renderFirstChild(tree.children[0])
+                        , *chain(*map(renderMiddleChildren, tree.children[1:-1]))
+                        , *renderLastChild(tree.children[-1])
+                        ]
+                else: #len(tree.children)==1
+                    return renderSoleChild(tree.children[0])
+
+        return "\n".join(render(self))
 
 
 def posToken(pos:str):
@@ -126,10 +173,6 @@ tt = phrase( 't'
 
 
     
-
-# use this string method: string.ljust(width, fillchar)    
-# >>> words = ['this', 'is', 'a', 'sentence']
-# >>> '-'.join(words)
 sample1 = RoseTree('P',
     RoseTree('P1', 
         RoseTree('1'),
@@ -154,108 +197,4 @@ sample1 = RoseTree('P',
 #     |____7
 
 
-# P___P1_____________1 : P P1 1        ctx: [(P,'|'), (P1,'|')] -> 
-# |   |______________2 : ? |_2         ctx: [(P,'|'), '|']
-# |___P2_____________3 : | P2_3
-# |___P3___P4________4 : | P3_P4_4
-#     |    |____P5___5 : ? ?  |_P5_5   -> passing the context of P:' ',P3:'|'
-#     |         |____6 : ? ? ? |_6
-#     |______________7 : ? |_7
-
-
-
-# NodeInfo = str
-# #[MID],[END],or the label of the node
-# _MID = "[MID]"
-# _END = "[END]"
-
-# @dataclass
-# class Context:
-#     nodeInfo : NodeInfo
-#     padding : int
-
-T = TypeVar('T')
-def rindex(pred:Callable[[T],bool],l:Sequence[T])->int:
-    for i in range(len(l)-1, -1, -1): # Iterating index in reverse order.
-        if pred(l[i]):
-            return i
-    return -1
-
-# def strParseTree(tree:RoseTree, indent=0, alignLeaves=0, layerLength=6)->str:
-#     def collectPathInfos(t:RoseTree,ctxs=[])->list[tuple[list[Context],str]]:
-#         if len(t.children)==0:
-#             return [(ctxs,str(t.content))]
-#         else:
-#             padding = 0 if len(tree.content)<layerLength else len(tree.content)+1
-#             head = collectPathInfos(t.children[0], ctxs+[Context(str(t.content), padding)])
-#             middles = list(chain(*map( partial(collectPathInfos,ctxs=ctxs+[Context(_MID,padding)])
-#                                      , t.children[1:-1])))
-#             last= collectPathInfos(t.children[-1], ctxs+[Context(_END,padding)]) if len(t.children)>1 else []
-#             return head + middles + last
-#     pathInfos = collectPathInfos(tree)
-
-#     def renderLeaf(ctxs:list[Context], leafContent:str)->Iterable[str]:
-#         # The rule for horz: scan from right, horz line stops at first non-HEAD (MID or END).
-#         # The rules for vert: only when END should it be " " except the next ctx is HEAD, otherwise "|".
-        
-#         # horz-rule: splitting
-#         splitIndex = rindex(lambda x:x.nodeInfo in [_MID,_END],ctxs)
-#         splitIndex = 0 if splitIndex==-1 else splitIndex
-#         #splitIndex==-1 means no non-head, so every segment should be rendered with horz line.
-#         print(leafContent)
-#         print(ctxs)
-#         # vert-rule
-#         def vertSymb(i:int,printHead:bool)->str:
-#             if printHead and ctxs[i].nodeInfo not in [_MID,_END]:
-#                 return ctxs[i].nodeInfo
-#             elif ctxs[i].nodeInfo==_END and\
-#                  i+1<len(ctxs) and (ctxs[i+1].nodeInfo in [_MID,_END]):
-#                 return " "
-#             else:
-#                 return "|"
-#         return (*(vertSymb(i,False).ljust(layerLength+ctxs[i].padding," ") for i in range(0,splitIndex))
-#                ,*(vertSymb(i,True).ljust(layerLength+ctxs[i].padding,"_") for i in range(splitIndex,len(ctxs)))
-#                ,leafContent
-#                ,'\n'
-#                )
-#     return ''.join(chain(*starmap(renderLeaf,pathInfos)))
-
-        
-
-
-# def strParseTree(tree:RoseTree, indent=0, padding=0, alignLeaves=0, layerLength=6): #->list[str]
-#     def collectInfo
-#     if len(tree.children)==0:
-#         return [str(tree.content)]
-#     else:
-#         contLen = len(tree.content)
-#         # padding = contLen-layerLength+1 if contLen>=layerLength else padding
-#         #         # +1 is an additional "_" character to separate parent and child nodes
-#         childrenStr = [strParseTree(i, indent=indent+1) for i in tree.children]
-
-#         contentStr = str(tree.content).ljust(layerLength, '_')
-#         if contLen>=layerLength:
-#             contentStr+='_'
-#         r_firstLine:str = contentStr + childrenStr[0][0]
-
-#         def prepending(vertBar:bool):
-
-
-#         localPaddingForFirstChild = (' ' if len(childrenStr)<2 else '|').ljust(layerLength,'_')
-#         r_firstChildsLines:list[str] = [
-#             ' '*indent*layerLength + localPaddingForFirstChild + firstChildsChild
-#             for firstChildsChild in childrenStr[0]
-#         ]
-
-#         r_middleChildrenLines:list[str] = list(itertools.chain(*[
-#             ['a','b']
-#             for middleChild in childrenStr[1:-1]
-#         ]))
-
-#         r_lastChildsLines:list[str]
-
-
-        # Another way of doing this: use DFS order to fetch each node's information, 
-        # then format each line at once (in a single loop).
-        # I guess this could reduce string manipulations, which make the method more efficient.
 
